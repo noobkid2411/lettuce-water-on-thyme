2c99d0f9b2d1f98e30a4b9076e5ba3df5df8103f
noobkid2411 <sonakshimishra29@gmail.com>
2021-06-19 18:34:28 +0530

    Updated after firebase hosting

--- public/App.js
+++ public/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firebase from './firebase1.js';
+import firebase from './firebase.js';
 console.log(logo);
 class App extends Component {
   componentDidMount() {
